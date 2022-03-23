4abf36065568c21af2b56aa6f3da6bebc98666bf
Chang-Man <changagim@snu.ac.kr>
2022-03-23 20:49:42 +0900

    create common folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import Layout from './components/layout/Layout';
+import Layout from './components/common/layout/Layout';
 import Schedule from './components/schedule/Schedule';
 import SnudySpace from './components/snudyspace/SnudySpace';
 import SubjectDetail from './components/snudyspace/subject-detail/SubjectDetail';
