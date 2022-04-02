42d272d126e1452831758762c54a0d45c59260a0
woohm402 <woohm404@gmail.com>
2022-04-02 11:05:30 +0900

    fix typo

--- src/components/example/Example.msw.ts
+++ src/components/example/Example.msw.ts
@@ -3,7 +3,7 @@ import { rest } from 'msw';
 import { BASE_URL } from '../../apis/baseURL';
 
 /**
- * example component for mocking
+ * example for mocking
  */
 
 const getExample = rest.get(`${BASE_URL}/example`, (req, res, ctx) => res(ctx.json(['사과', '바나나'])));
